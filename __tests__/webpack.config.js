6af194f283cd946d469f8ee7c3d8d9b188dc5859
Melvin Mawejje <melvinmawejje@gmail.com>
2024-09-27 18:50:29 +0300

    Added babelrc

--- __tests__/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './client/src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'client/src/index.js'),
-  },
-};
