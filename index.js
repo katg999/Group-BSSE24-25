c39732814a4ea8675e8ba7b96e26150702f067ef
katg999 <katex911@gmail.com>
2024-10-10 11:50:33 +0300

    Deploy

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDb = require('./config/connectDb');
-const itemRoutes = require('./routes/itemRoutes');
-const userRoutes = require('./routes/userRoute');
-const billsRoutes = require('./routes/billsRoute');
-const path = require('path');
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url'; // To get __dirname in ES6 modules
+import connectDb from './config/connectDb.js'; // Add .js extension in ES6 imports
+import itemRoutes from './routes/itemRoutes.js';
+import userRoutes from './routes/userRoute.js';
+import billsRoutes from './routes/billsRoute.js';
 
 dotenv.config();
 connectDb();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
+// Routes
 app.use('/api/items', itemRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/bills', billsRoutes);
 
-// static files
+// Serve static files from the React app
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, './client/build')));
 
-app.get('*', function (req, res) {
+// Catch-all for serving React app
+app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
+// Set up the PORT
 const PORT = process.env.PORT || 4001;
-
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
 });
