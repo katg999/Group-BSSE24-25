b368be8bec51c088052866cab3fae78db951f08a
Solomon Spencer <solomonjass4@gmail.com>
2024-10-11 14:46:40 +0300

    customer changes

--- client/src/pages/CustomerPage.js
+++ client/src/pages/CustomerPage.js
@@ -1,49 +1,47 @@
 import { Table } from 'antd';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react'; // Import useCallback
 import { useDispatch } from 'react-redux';
 import DefaultLayout from '../components/DefaultLayout';
 
 const CustomerPage = () => {
-	const [billsData, setBillsData] = useState([]);
-	const dispatch = useDispatch();
+  const [billsData, setBillsData] = useState([]);
+  const dispatch = useDispatch();
 
-	const getAllBills = async () => {
-		try {
-			dispatch({ type: 'SHOW_LOADING' });
-			const { data } = await axios.get('/api/bills/get-bills');
-			setBillsData(data);
-		} catch (error) {
-			console.error('Error fetching bills:', error);
-		} finally {
-			dispatch({ type: 'HIDE_LOADING' });
-		}
-	};
+  const getAllBills = useCallback(async () => { // Use useCallback here
+    try {
+      dispatch({ type: 'SHOW_LOADING' });
+      const { data } = await axios.get('/api/bills/get-bills');
+      setBillsData(data);
+    } catch (error) {
+      console.error('Error fetching bills:', error);
+    } finally {
+      dispatch({ type: 'HIDE_LOADING' });
+    }
+  }, [dispatch]); // Add dispatch as a dependency
 
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    getAllBills(); 
+  }, [getAllBills]); // Include getAllBills in the dependency array
 
-	useEffect(() => {
-		getAllBills();
-	}, []); // Empty dependency array to call once on component mount
+  const columns = [
+    { title: 'ID', dataIndex: '_id' },
+    { title: 'Customer Name', dataIndex: 'customerName' },
+    { title: 'Contact No', dataIndex: 'customerNumber' },
+    { title: 'Total Amount', dataIndex: 'totalAmount' },
+  ];
 
-	const columns = [
-		{ title: 'ID', dataIndex: '_id' },
-		{ title: 'Customer Name', dataIndex: 'customerName' }, // Fixed typo in "Customer"
-		{ title: 'Contact No', dataIndex: 'customerNumber' },
-		{ title: 'Total Amount', dataIndex: 'totalAmount' },
-	];
-
-	return (
-		<DefaultLayout>
-			<h1>Customer Page</h1>
-			<Table
-				columns={columns}
-				dataSource={billsData}
-				bordered
-				pagination={false}
-			/>
-		</DefaultLayout>
-	);
+  return (
+    <DefaultLayout>
+      <h1>Customer Page</h1>
+      <Table
+        columns={columns}
+        dataSource={billsData}
+        bordered
+        pagination={false}
+      />
+    </DefaultLayout>
+  );
 };
 
 export default CustomerPage;
