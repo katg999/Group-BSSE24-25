a274f74a077bfb028669b9cac261b718b030e5a3
Solomon Spencer <solomonjass4@gmail.com>
2024-10-11 11:46:27 +0300

    tests

---
+++ client/tests/bills.test.js
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import BillsPage from './BillsPage';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+
+const mockStore = configureStore([]);
+
+describe('BillsPage Component', () => {
+	let store;
+
+	beforeEach(() => {
+		store = mockStore({
+			// Mock any necessary store states here, for example:
+			loading: false,
+		});
+	});
+
+	test('renders the table with no data', () => {
+		render(
+			<Provider store={store}>
+				<BillsPage />
+			</Provider>
+		);
+
+		const invoiceHeader = screen.getByText(/invoice list/i);
+		expect(invoiceHeader).toBeInTheDocument();
+
+		const table = screen.getByRole('table');
+		expect(table).toBeInTheDocument();
+	});
+});
