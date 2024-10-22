8afcb97435d73e766e23140d25968f163763da1a
Melvin Mawejje <melvinmawejje@gmail.com>
2024-10-22 13:47:28 +0300

    C

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,19 @@
-import globals from "globals";
-import js from "@eslint/js";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-
-export default [
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    languageOptions: { 
-      globals: {
-        ...globals.browser
-      },
-      parserOptions: {
-        ecmaVersion: 2022,
-        sourceType: "module",
-        ecmaFeatures: {
-          jsx: true
-        }
-      }
-    },
-    plugins: {
-      react,
-      "react-hooks": reactHooks
-    },
-    rules: {
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn"
+{
+  "extends": ["react-app"],
+  "parserOptions": {
+    "ecmaVersion": 2022,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
     }
+  },
+  "env": {
+    "browser": true,
+    "node": true,
+    "es2022": true
+  },
+  "rules": {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
   }
-];
+}
