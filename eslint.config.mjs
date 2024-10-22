b6747b1706d5d9fed39cf3a3fb9ff1a6bc4c7890
Melvin Mawejje <melvinmawejje@gmail.com>
2024-10-22 12:33:09 +0300

    Add npmc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,55 +1,30 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 
-const config = [
+export default [
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     languageOptions: { 
-      globals: globals.browser,
+      globals: {
+        ...globals.browser
+      },
       parserOptions: {
-        ecmaVersion: "latest",
+        ecmaVersion: 2022,
         sourceType: "module",
         ecmaFeatures: {
           jsx: true
         }
       }
     },
-    settings: {
-      react: {
-        version: "detect"
-      }
+    plugins: {
+      react,
+      "react-hooks": reactHooks
     },
     rules: {
-      // Disable all rules from pluginJs.configs.recommended
-      ...Object.fromEntries(
-        Object.keys(pluginJs.configs.recommended.rules || {}).map(rule => [rule, "off"])
-      ),
-      // Disable all rules from pluginReact.configs.flat.recommended
-      ...Object.fromEntries(
-        Object.keys(pluginReact.configs.flat.recommended.rules || {}).map(rule => [rule, "off"])
-      ),
-      // Only enable parsing errors
-      "no-unused-vars": "off",
-      "no-undef": "off"
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn"
     }
   }
 ];
-
-// Conditionally add react-hooks rules if the plugin is available
-try {
-  const pluginHooks = await import("eslint-plugin-react-hooks");
-  config[0].plugins = {
-    react: pluginReact,
-    "react-hooks": pluginHooks.default
-  };
-  config[0].rules = {
-    ...config[0].rules,
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
-  };
-} catch (error) {
-  console.warn("eslint-plugin-react-hooks not found, skipping hooks-related rules");
-}
-
-export default config;
